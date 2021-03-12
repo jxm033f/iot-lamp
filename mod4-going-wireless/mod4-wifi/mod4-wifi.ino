2ffd68c17e74348f75ea72f04e1d68b446515094
Jenny Martinez <jxm033f@gmail.com>
2021-03-12 13:17:17 -0500

    Troubleshooting

--- mod4-going-wireless/mod4-wifi/mod4-wifi.ino
+++ mod4-going-wireless/mod4-wifi/mod4-wifi.ino
@@ -1,4 +1,3 @@
-#include "Arduino.h"
 #include <WebServer.h>
 #include <WiFi.h>
 #include <WiFiUdp.h>
@@ -6,7 +5,7 @@
 #define GREEN_LED1      14
 #define GREEN_LED2      13
 #define BLUE_LED1       12
-#define BLUE_LED2       9
+#define BLUE_LED2       27
 #define YELLOW_LED1     19
 #define YELLOW_LED2     4
 #define RED_LED1        18
@@ -22,15 +21,12 @@ int analogValue;
 String colorString = "";
 
 // the IP of the machine to which you send msgs - this should be the correct IP in most cases (see note in python code)
-#define CONSOLE_IP "192.168.1.2"
-#define CONSOLE_PORT 4210
-const char* ssid = "ESP32";
-const char* password = "12345678";
+const char* ssid = "Columbia University";
+const char* password = "";
 WiFiUDP Udp;
-IPAddress local_ip(192, 168, 1, 1);
-IPAddress gateway(192, 168, 1, 1);
-IPAddress subnet(255, 255, 255, 0);
-WebServer server(80);
+
+unsigned int localUdpPort = 4210;  //  port to listen on
+char incomingPacket[255];  // buffer for incoming packets
 
 void clear_leds() {
   digitalWrite(BLUE_LED1, LOW);
@@ -44,6 +40,8 @@ void clear_leds() {
 }
 
 void setup() {
+  int status = WL_IDLE_STATUS;
+  
   Serial.begin(115200);
 
   pinMode(WEATHER_BUTTON, INPUT);
@@ -58,82 +56,87 @@ void setup() {
   pinMode(RED_LED1, OUTPUT);
   pinMode(RED_LED2, OUTPUT);
   
-  WiFi.softAP(ssid, password);
-  WiFi.softAPConfig(local_ip, gateway, subnet);
-  server.begin();
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println("Connected to wifi");
+  Udp.begin(localUdpPort);
+  Serial.printf("Now listening at IP %s, UDP port %d\n", WiFi.localIP().toString().c_str(), localUdpPort);
 }
 
 void loop() {
   analogValue = analogRead(PHOTORESISTOR) / 4;
 
-  Udp.beginPacket(CONSOLE_IP, CONSOLE_PORT);
-  Udp.print(analogValue);
+  Udp.beginPacket(Udp.remoteIP(), Udp.remotePort());
+  Udp.printf(analogValue);
   Udp.endPacket();
 
-  if (WEATHER_BUTTON == LOW && weatherIsPressed) {
-    weatherIsPressed = !weatherIsPressed;
-    delay(100);
-
-    if (!weatherAPIOn) {
-      prevAPIStatus = weatherAPIOn;
-      Serial.println("Turn on API");
-      weatherAPIOn = !weatherAPIOn;
-    } else {
-      prevAPIStatus = weatherAPIOn;
-      weatherAPIOn = !weatherAPIOn;
-    }
-  } else if (WEATHER_BUTTON == HIGH && !weatherIsPressed) {
-    weatherIsPressed = !weatherIsPressed;
-  }
-
-  while (Serial.available() > 0) {
-    char c = Serial.read();
-    colorString = c;
-  }
-
-  if (prevAPIStatus != weatherAPIOn) {
-    clear_leds();
-  }
-  
-  if (!weatherAPIOn) {
-    if (analogValue < 127) {
-      digitalWrite(YELLOW_LED1, HIGH);
-      digitalWrite(YELLOW_LED2, HIGH);
-    } else {
-      digitalWrite(YELLOW_LED1, LOW);
-      digitalWrite(YELLOW_LED2, LOW);
-    }
-  } else {
-    if (analogValue < 127) {
-      if (colorString == "B") {
-        digitalWrite(BLUE_LED1, HIGH);
-        digitalWrite(BLUE_LED2, HIGH);
-      } else if (colorString == "G") {
-        digitalWrite(GREEN_LED1, HIGH);
-        digitalWrite(GREEN_LED2, HIGH);
-      } else if (colorString == "Y") {
-        digitalWrite(YELLOW_LED1, HIGH);
-        digitalWrite(YELLOW_LED2, HIGH);
-      } else if (colorString == "R") {
-        digitalWrite(RED_LED1, HIGH);
-        digitalWrite(RED_LED2, HIGH);
-      }
-    } else {
-      if (colorString == "B") {
-        digitalWrite(BLUE_LED1, LOW);
-        digitalWrite(BLUE_LED2, LOW);
-      } else if (colorString == "G") {
-        digitalWrite(GREEN_LED1, LOW);
-        digitalWrite(GREEN_LED2, LOW);
-      } else if (colorString == "Y") {
-        digitalWrite(YELLOW_LED1, LOW);
-        digitalWrite(YELLOW_LED2, LOW);
-      } else if (colorString == "R") {
-        digitalWrite(RED_LED1, LOW);
-        digitalWrite(RED_LED2, LOW);
-      }
-    }
-  }
+//  if (WEATHER_BUTTON == LOW && weatherIsPressed) {
+//    weatherIsPressed = !weatherIsPressed;
+//    delay(100);
+//
+//    if (!weatherAPIOn) {
+//      prevAPIStatus = weatherAPIOn;
+//      Serial.println("Turn on API");
+//      weatherAPIOn = !weatherAPIOn;
+//    } else {
+//      prevAPIStatus = weatherAPIOn;
+//      weatherAPIOn = !weatherAPIOn;
+//    }
+//  } else if (WEATHER_BUTTON == HIGH && !weatherIsPressed) {
+//    weatherIsPressed = !weatherIsPressed;
+//  }
+//
+//  while (Serial.available() > 0) {
+//    char c = Serial.read();
+//    colorString = c;
+//  }
+//
+//  if (prevAPIStatus != weatherAPIOn) {
+//    clear_leds();
+//  }
+//  
+//  if (!weatherAPIOn) {
+//    if (analogValue < 127) {
+//      digitalWrite(YELLOW_LED1, HIGH);
+//      digitalWrite(YELLOW_LED2, HIGH);
+//    } else {
+//      digitalWrite(YELLOW_LED1, LOW);
+//      digitalWrite(YELLOW_LED2, LOW);
+//    }
+//  } else {
+//    if (analogValue < 127) {
+//      if (colorString == "B") {
+//        digitalWrite(BLUE_LED1, HIGH);
+//        digitalWrite(BLUE_LED2, HIGH);
+//      } else if (colorString == "G") {
+//        digitalWrite(GREEN_LED1, HIGH);
+//        digitalWrite(GREEN_LED2, HIGH);
+//      } else if (colorString == "Y") {
+//        digitalWrite(YELLOW_LED1, HIGH);
+//        digitalWrite(YELLOW_LED2, HIGH);
+//      } else if (colorString == "R") {
+//        digitalWrite(RED_LED1, HIGH);
+//        digitalWrite(RED_LED2, HIGH);
+//      }
+//    } else {
+//      if (colorString == "B") {
+//        digitalWrite(BLUE_LED1, LOW);
+//        digitalWrite(BLUE_LED2, LOW);
+//      } else if (colorString == "G") {
+//        digitalWrite(GREEN_LED1, LOW);
+//        digitalWrite(GREEN_LED2, LOW);
+//      } else if (colorString == "Y") {
+//        digitalWrite(YELLOW_LED1, LOW);
+//        digitalWrite(YELLOW_LED2, LOW);
+//      } else if (colorString == "R") {
+//        digitalWrite(RED_LED1, LOW);
+//        digitalWrite(RED_LED2, LOW);
+//      }
+//    }
+//  }
   
   delay(1000);
 }
--- mod4-going-wireless/mod4-wireless.py
+++ mod4-going-wireless/mod4-wireless.py
@@ -1,4 +1,6 @@
+import serial
 import socket
+import time
 
 #api and location information
 api_key = "490a5a33da6ecfbad4c3dc91138d9a12"
@@ -6,33 +8,34 @@ lat = 40.712776
 lon = -74.005974
 url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=imperial" % (lat, lon, api_key)
 
-LOCAL_UDP_IP = "192.168.1.2"
+UDP_IP = "160.39.190.130"
 SHARED_UDP_PORT = 4210
 
-arduino = serial.Serial('/dev/ttyUSB0', 9600)
-time.sleep(2)
-
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((LOCAL_UDP_IP, SHARED_UDP_PORT))
+sock.connect((UDP_IP, SHARED_UDP_PORT))
 
-def get_connection():
-	response = requests.get(url)
-	data = json.loads(response.text)
-	current_temp = data["current"]["temp"]
-	tensdigit = int(current_temp//10)
-	return tensdigit
+# #arduino = serial.Serial('/dev/ttyUSB0', 9600)
+# arduino = serial.Serial('/dev/cu.wchusbserial14410', 115200)
+# time.sleep(2)
+
+# def get_connection():
+# 	response = requests.get(url)
+# 	data = json.loads(response.text)
+# 	current_temp = data["current"]["temp"]
+# 	tensdigit = int(current_temp//10)
+# 	return tensdigit
 
 def loop():
     while True:
-        data, addr = sock.recvfrom(2048)
+        data = sock.recv(2048)
         print(data)
 
-        if str(arduino.readline()).find("Turn on API") != -1:
-        	digitTemp = get_connection()
-        	tempLetter = ["B", "B", "G", "G", "Y", "Y", "R", "R"]
-        	arduino.write(tempLetter[digitTemp].encode())
+        # if str(arduino.readline()).find("Turn on API") != -1:
+        # 	digitTemp = get_connection()
+        # 	tempLetter = ["B", "B", "G", "G", "Y", "Y", "R", "R"]
+        # 	arduino.write(tempLetter[digitTemp].encode())
 
 
 
 if __name__ == "__main__":
-    loop()
+	loop()
